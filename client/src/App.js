54b4797429384beecd23ddd5e80786ad419ad6c6
burhan.amk1 <burhan.amk1@gmail.com>
2023-05-12 17:02:36 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = 'Google Api Client Id';
+
+  
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
